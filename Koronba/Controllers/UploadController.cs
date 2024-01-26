178644f19d1a178f44bcd85b40ef8de05dae34ec
naomiEve <0xlunaric@gmail.com>
2024-01-27 00:28:12 +0100

    feat: start working on uploading.

---
+++ Koronba/Controllers/UploadController.cs
@@ -0,0 +1,41 @@
+﻿using Koronba.Core.Persistence.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Koronba.Controllers;
+
+/// <summary>
+/// The controller responsible for handling file upload.
+/// </summary>
+[Route("/upload")]
+public class UploadController(
+    IFlashUploadService uploadService) : Controller
+{
+    /// <summary>
+    /// The flash upload service.
+    /// </summary>
+    private readonly IFlashUploadService _flashUploadService = uploadService;
+    
+    /// <summary>
+    /// The index for the upload page.
+    /// </summary>
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    /// <summary>
+    /// The POST upload method.
+    /// </summary>
+    /// <param name="file">The file to be uploaded.</param>
+    [HttpPost]
+    [Route("/upload")]
+    public async Task<IActionResult> Upload([FromForm] IFormFile file)
+    {
+        var result = await _flashUploadService.Upload(new(file.FileName, file.OpenReadStream()));
+        if (result is null)
+            return StatusCode(StatusCodes.Status500InternalServerError);
+
+        return Accepted();
+    }
+}
